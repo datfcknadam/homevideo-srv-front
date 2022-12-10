660f8658d74e7f49f223e0f5b428e6b74beb616d
Egor Kulikov <benstiller68@gmail.com>
2022-12-11 00:45:40 +0300

    init

--- src/store/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import movieReducer  from './movie/reducer';
-
-const rootReducer = combineReducers({
-    movie: movieReducer,
-})
- 
-export default rootReducer;
